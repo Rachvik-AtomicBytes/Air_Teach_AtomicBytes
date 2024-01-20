d186ebcaa7dea81c70f0a381519932ba4fda716c
Rachvik-AtomicBytes <157193388+Rachvik-AtomicBytes@users.noreply.github.com>
2024-01-20 22:31:15 +0530

    Create Arduino_Code.ino
    Code for the Student Motion Detector

---
+++ Arduino_Code.ino
@@ -0,0 +1,74 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(13, 12, 8, 7, 4, 2);
+
+int sensorPin = A0;
+int buzzerPin = 13;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(sensorPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  digitalWrite(buzzerPin, LOW);
+  lcd.print("Welcome");
+  lcd.setCursor(0, 1);
+  lcd.print("Students");
+  delay(4000);
+  lcd.clear();
+}
+
+void loop() {
+  int sensorValue = digitalRead(sensorPin);
+  int buzzerValue = digitalRead(buzzerPin);
+  digitalWrite(buzzerPin, LOW);
+
+  if (sensorValue == HIGH) {
+    lcd.setCursor(0, 0);
+    lcd.print("Student at work");
+    delay(2000);
+    if (sensorValue == LOW) {
+      lcd.print("No contact");
+      lcd.setCursor(0, 1);
+      lcd.print("with table");
+      delay(2000);
+    }
+    unsigned long startTime = millis();
+    unsigned long delayDuration = 30000;  // 30 seconds
+
+    while (millis() - startTime < delayDuration) {
+      if (sensorValue == HIGH) {
+        digitalWrite(buzzerPin, LOW);
+      }
+      // Additional conditions and actions here if needed
+    }
+
+    
+    lcd.clear();
+  } else if (sensorValue == LOW) {
+    lcd.setCursor(0, 0);
+    lcd.print("No contact ");
+    lcd.setCursor(0, 1);
+    lcd.print("with table");
+    delay(2000);
+    if (sensorValue == HIGH) {
+      lcd.print("Student at work");
+      delay(2000);
+    }
+    
+    unsigned long startTime = millis();
+    unsigned long delayDuration = 30000;  // 30 seconds
+
+    while (millis() - startTime < delayDuration) {
+      if (sensorValue == LOW) {
+        digitalWrite(buzzerPin, LOW);
+      }
+      // Additional conditions and actions here if needed
+    }
+
+    
+    lcd.clear();
+  }
+}
